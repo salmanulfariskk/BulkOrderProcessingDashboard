1ab68ec9648d96b72d93e6fa460ae5d7816e2167
salmanulfariskk <salmanulfaris094@gmail.com>
2026-02-23 15:49:48 +0530

    update socket.io cors config to strip trailing slash

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -5,9 +5,10 @@ import jwt from 'jsonwebtoken';
 let io: SocketIOServer;
 
 export const initSocket = (server: HTTPServer) => {
+    const allowedOrigin = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.replace(/\/$/, '') : 'http://localhost:3000';
     io = new SocketIOServer(server, {
         cors: {
-            origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+            origin: [allowedOrigin, 'http://localhost:3000'],
             methods: ["GET", "POST"]
         }
     });
